93dcda33d1ae51ba09c4393ea7c820fa479f3572
Erin Harris <eharris@ca.ibm.com>
2020-02-05 10:21:23 -0500

    CW Issue #2033: Update links to documentation

--- dev/org.eclipse.codewind.ui/src/org/eclipse/codewind/ui/internal/UIConstants.java
+++ dev/org.eclipse.codewind.ui/src/org/eclipse/codewind/ui/internal/UIConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,7 @@ public class UIConstants {
 	public static final String 
 		DOC_BASE_URL = "https://www.eclipse.org/codewind",
 		CWSETTINGS_INFO_URL = DOC_BASE_URL + "/mdteclipsemanagingprojects.html",
-		TEMPLATES_INFO_URL = DOC_BASE_URL + "/mdteclipseworkingwithtemplates.html",
+		TEMPLATES_INFO_URL = DOC_BASE_URL + "/workingwithtemplates.html",
 		REGISTRY_INFO_URL = DOC_BASE_URL + "/image-registry-credentials.html",
-		REMOTE_SETUP_URL =  DOC_BASE_URL +"/remoteconnectionui.html";
+		REMOTE_SETUP_URL =  DOC_BASE_URL +"/remotedeploy-eclipse.html";
 }

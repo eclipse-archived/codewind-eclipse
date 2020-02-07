75737986dd0564db948e74ecd8bbba2363044399
Erin Harris <eharris@ca.ibm.com>
2020-02-07 10:21:53 -0500

    CW Issue #1812: Use the notification id to only show notifications once

--- dev/org.eclipse.codewind.core/src/org/eclipse/codewind/core/internal/constants/CoreConstants.java
+++ dev/org.eclipse.codewind.core/src/org/eclipse/codewind/core/internal/constants/CoreConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 IBM Corporation and others.
+ * Copyright (c) 2018, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -77,7 +77,7 @@ public class CoreConstants {
 			KEY_APP_STATUS = "appStatus",
 			KEY_DETAILED_APP_STATUS = "detailedAppStatus",
 			KEY_MESSAGE = "message",
-			KEY_NOTIFY = "notify",
+			KEY_NOTIFICATION_ID = "notificationID",
 			KEY_LINK = "link",
 			KEY_LINK_LABEL = "linkLabel",
 			KEY_BUILD_STATUS = "buildStatus",

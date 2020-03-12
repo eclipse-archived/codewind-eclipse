b28e6c93bb0aa3361d8551315b76d21521e0657a
Erin Harris <eharris@ca.ibm.com>
2020-03-12 15:55:54 -0400

    CW Issue #2471: Generate an install zip for test plugins and features

---
+++ dev/org.eclipse.codewind.test/src/org/eclipse/codewind/test/AllTests.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.codewind.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	ExternalLinkTest.class
+})
+
+public class AllTests {
+	// Empty
+}
